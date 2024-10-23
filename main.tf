858c1b92cbfc54a920e9acbfd1b614f5fdfc05af
Anjith Paul <anjithpaul@Anjiths-MacBook-Pro.local>
2024-10-23 17:38:39 +0530

    add vpc

--- main.tf
+++ main.tf
@@ -1,8 +1,39 @@
-resource "aws_instance" "app_server" {
-  ami           =  "ami-04a37924ffe27da53"
-  instance_type = "t2.micro"
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "192.168.3.0/25"
+  tags = {
+    Name = "ec2_iac_vpc"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "192.168.3.0/26"
+  availability_zone = "ap-south-1"
 
   tags = {
-    Name = "backend_server"
+    Name = "ec2_iac_subnet"
+  }
+}
+
+resource "aws_network_interface" "foo" {
+  subnet_id   = aws_subnet.my_subnet.id
+  private_ips = ["192.168.3.1"]
+
+  tags = {
+    Name = "ec2_iac_network_interface"
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-04a37924ffe27da53"
+  instance_type = "t2.micro"
+
+  network_interface {
+    network_interface_id = aws_network_interface.foo.id
+    device_index         = 0
+  }
+
+  tags = {
+    Name = "ec2_iac_backend_server"
   }
 }
