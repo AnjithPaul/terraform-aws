de75276f2903bfb833edacbb702ccb47ba57fda0
Anjith Paul <anjithpaul@Anjiths-MacBook-Pro.local>
2024-10-23 17:25:02 +0530

    add remote backend

--- providers.tf
+++ providers.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region = "ap-south-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "ec2-iac-state"
+    key            = "global/s3/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "ec2-iac-statelock"
+    encrypt        = true
+  }
+}
